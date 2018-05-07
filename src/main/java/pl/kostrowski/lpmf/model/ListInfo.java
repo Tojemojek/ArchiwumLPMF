fc27bc370182ccfb523f3ca147e2fffa25e761a1
Krzysztof <inz@kostrowski.pl>
2018-05-07 22:05:30 +0200

    Poprawka ListInfo

--- src/main/java/pl/kostrowski/lpmf/model/ListInfo.java
+++ src/main/java/pl/kostrowski/lpmf/model/ListInfo.java
@@ -1,19 +1,53 @@
 package pl.kostrowski.lpmf.model;
 
-import org.hibernate.annotations.GenericGenerator;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import java.util.List;
+import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
-@Table(name = "fullLists")
-public class FullList {
+@Table(name = "list_info")
+public class ListInfo{
 
     @Id
     @Column(name = "noOfList")
     private Integer noOfList;
 
     @Column(name = "date")
-    private String date;
+    private LocalDate date;
 
+    public ListInfo() {
+    }
+
+    public Integer getNoOfList() {
+        return noOfList;
+    }
+
+    public void setNoOfList(Integer noOfList) {
+        this.noOfList = noOfList;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListInfo listInfo = (ListInfo) o;
+        return Objects.equals(noOfList, listInfo.noOfList) &&
+                Objects.equals(date, listInfo.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(noOfList, date);
+    }
 }
