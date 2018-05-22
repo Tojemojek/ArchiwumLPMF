d77bff663163adb5b96690d5fbf0779af732c422
Krzysztof <inz@kostrowski.pl>
2018-05-22 21:19:17 +0200

    Approach towards presentation of data

--- src/main/java/pl/kostrowski/lpmf/controller/MainController.java
+++ src/main/java/pl/kostrowski/lpmf/controller/MainController.java
@@ -6,7 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.kostrowski.lpmf.model.LPMFPosition;
+import pl.kostrowski.lpmf.model.Song;
 import pl.kostrowski.lpmf.repository.LPMFPositionRepository;
+import pl.kostrowski.lpmf.repository.SongRepository;
 
 import java.util.List;
 
@@ -15,6 +17,8 @@ public class MainController {
 
     @Autowired
     LPMFPositionRepository lpmfPositionRepository;
+    @Autowired
+    SongRepository songRepository;
 
 
     @RequestMapping(value = "/")
@@ -37,8 +41,21 @@ public class MainController {
                                  @RequestParam(value = "toList", defaultValue = "1") Integer toList,
                                  Model model) {
         List<LPMFPosition> allByNoOfListOOrderByPos = lpmfPositionRepository.findByNoOfList(fromList);
-        model.addAttribute("lists",allByNoOfListOOrderByPos);
+        model.addAttribute("lists", allByNoOfListOOrderByPos);
         return "singleList";
     }
 
+    @RequestMapping(value = "/songPos")
+    public String showMe(@RequestParam(value = "songId", defaultValue = "32") Integer songId, Model model) {
+        List<LPMFPosition> findOne = lpmfPositionRepository.customFindBySong(Long.valueOf(songId));
+        model.addAttribute("singleSong",findOne);
+        return "songPos";
+    }
+
+    @RequestMapping(value = "/allSongs")
+    public String showAllSongs(@RequestParam(value = "songId", defaultValue = "32") Integer songId, Model model) {
+        List<Song> allSongs = songRepository.findAll();
+        model.addAttribute("allSongs", allSongs);
+        return "allSongs";
+    }
 }
--- src/main/java/pl/kostrowski/lpmf/repository/LPMFPositionRepository.java
+++ src/main/java/pl/kostrowski/lpmf/repository/LPMFPositionRepository.java
@@ -1,6 +1,8 @@
 package pl.kostrowski.lpmf.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.kostrowski.lpmf.model.LPMFPosition;
 
@@ -15,4 +17,6 @@ public interface LPMFPositionRepository extends CrudRepository<LPMFPosition, Lon
 
     LPMFPosition findByNoOfListAndPos(Integer pos, Integer noOfList);
 
+    @Query("SELECT lpmf FROM LPMFPosition lpmf left join lpmf.song as s where s.id = :songId")
+    List<LPMFPosition> customFindBySong(@Param("songId") Long songId);
 }
